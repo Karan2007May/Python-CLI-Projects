dc0d55a6d38632fb0322308dc33c47548d684b4f
Karan Gattani <karangattani07@gmail.com>
2025-09-04 15:47:22 +0530

    Update mdtohtml.py

--- mdtohtml.py
+++ mdtohtml.py
@@ -1,1 +1,9 @@
+import markdown
 
+def convert_md_to_html():
+    md_text = """# Hello World
+This is a markdown file!"""
+    html = markdown.markdown(md_text)
+    print(html)
+
+convert_md_to_html()
